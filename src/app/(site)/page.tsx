3c5eaf69069a46a2d9faf7ab73024d21804573a3
YusukeNaito08 <yn10377110@gmail.com>
2026-04-27 18:46:48 +0900

    add:setup

---
+++ src/app/(site)/page.tsx
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+const Home = () => {
+  return (
+    <div>
+      <Title>
+        Yusuke Naito <br />
+        Portfolio
+      </Title>
+    </div>
+  );
+};
+
+export default Home;
+
+const Title = styled.h1``;
---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer>
+      <nav></nav>
+      <p>© {new Date().getFullYear()} Yusuke Naito</p>
+    </footer>
+  );
+}
